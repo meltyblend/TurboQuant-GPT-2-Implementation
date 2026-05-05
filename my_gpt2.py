202c8cedfd3267aeeee0e35dfe53e800cfc6007d
Aren <106656925+meltyblend@users.noreply.github.com>
2026-05-05 00:03:09 -0700

    requirements.txt added

--- my_gpt2.py
+++ my_gpt2.py
@@ -17,8 +17,8 @@ class Block(nn.Module):
         self.mlp = MLP(config)
 
     def forward(self, x):
-        x = x + self.attn(self.ln1(x))
-        x = x + self.mlp(self.ln2(x))
+        x = x + self.attn(self.ln1(x)) # residual around attention sub-layer
+        x = x + self.mlp(self.ln2(x))  # residual around MLP sub-layer
         return x
 
 
