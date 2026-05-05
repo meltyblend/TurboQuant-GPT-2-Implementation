2fb8a0eb101b099695bd06255ee0d205e3e1fa4a
meltyblend <aren.jpg@gmail.com>
2026-05-05 01:46:06 -0700

    this is our GPT Implementation

--- my_gpt2.py
+++ my_gpt2.py
@@ -9,22 +9,45 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 
-
+# multi-head masked self-attention
 class CausalSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+
+        # this is our output projection, maps concatenated heads back to n_embd
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
         self.c_proj.NANOGPT_SCALE_INIT = 1
 
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+
+    # attention computation
     def forward(self, x):
-        B, T, C = x.size()
-        qkv = self.c_attn(x)
-        q, k, v = qkv.split(C, dim=2)
-        k = k.view(B, T, C // 3, 3).transpose(1, 2)
-        q = q.view(B, T, C // 3, 3).transpose(1, 2)
-        v = v.view(B, T, C // 3, 3).transpose(1, 2)
+        B, T, C = x.size()# batch size, sequence length, embedding dimensionality (n_embd)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
+        # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs=C=768 channels in the Transformer
+        # directly from Andrej Karpathy's GPT-2 implementation comments
+
+        qkv = self.c_attn(x) # tensor of shape (B, T, 3 * n_embd)
+
+        q, k, v = qkv.split(self.n_embd, dim=2) # split into q, k, v tensors
+
+        # reshape q, k, v for multi-head attention
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        # attention(Q, K, V) = softmax((QK^T / sqrt(d_k))+M)V, M is a causal mask
+        # setting negative infinity to the upper triangular part of the attention matrix
+        # to prevent the model from attending to future tokens
         y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+
+        # merge heads and project back to n_embd
         y = y.transpose(1, 2).contiguous().view(B, T, C)
         y = self.c_proj(y)
         return y
