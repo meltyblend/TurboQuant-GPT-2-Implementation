f673291f94c1b578bc3fbce7997a4cba1b3c4c07
meltyblend <aren.jpg@gmail.com>
2026-05-05 07:36:59 -0700

    added the fineweb-edu dataset

--- my_gpt2.py
+++ my_gpt2.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # multi-head masked self-attention
 class CausalSelfAttention(nn.Module):
     def __init__(self, config):
@@ -54,6 +54,7 @@ class CausalSelfAttention(nn.Module):
         y = self.c_proj(y)
         return y
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class MLP(nn.Module):
     def __init__(self, config):
@@ -70,6 +71,7 @@ class MLP(nn.Module):
         x = self.c_proj(x)
         return x
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class Block(nn.Module):
     def __init__(self, config):
@@ -87,6 +89,7 @@ class Block(nn.Module):
         x = x + self.mlp(self.ln_2(x))  # residual around MLP sub-layer or FFN
         return x
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 @dataclass
 class GPTConfig:
@@ -96,6 +99,7 @@ class GPTConfig:
     n_head: int = 12        # 12 heads
     n_embd: int = 768       # embedding dimensionality
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class GPT(nn.Module):
     def __init__(self, config):
@@ -234,14 +238,14 @@ class GPT(nn.Module):
         ]
         num_decay_params = sum(p.numel() for p in decay_params)
         num_nodecay_params = sum(p.numel() for p in nodecay_params)
-        if master_process:
-            print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
-            print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        #if master_process:
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
         # Create AdamW optimizer and use the fused version if it is available
         fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
         use_fused = fused_available and device_type == "cuda"
-        if master_process:
-            print(f"using fused AdamW: {use_fused}")
+        #if master_process:
+        print(f"using fused AdamW: {use_fused}")
         optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
         return optimizer
 
@@ -283,6 +287,8 @@ class DataLoaderLite:
             self.current_position = 0
         return x, y
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 import time
 
@@ -295,18 +301,31 @@ elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"using device: {device}")
 
+
 torch.manual_seed(1337)
 if torch.cuda.is_available():
     torch.cuda.manual_seed(1337)
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+total_batch_size = 524288 # 2**19 ~ 0.5m in number of tokens
+B = 16 # micro batch size
+T = 1024 # sequence length
+assert total_batch_size % (B * T) == 0, "total_batch_size must be divisible by B(batch_size) * T(context_length)"
+grad_accum_steps = total_batch_size // (B * T)
+print(f"total desired batch size: {total_batch_size}")
+print(f"=> calculated grad_accum_steps: {grad_accum_steps}")
 
-train_loader = DataLoaderLite(B=16, T=1024)
+train_loader = DataLoaderLite(B=B, T=T)
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # all matmul operations will use the high precision setting and are being perfomed
 # on the GPU which is faster while using the tensor float32 precision
 # its avail on my 4070 super
 # usually his happens in aread where we use nn.linear
 torch.set_float32_matmul_precision('high')
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # getting logits
 #model = GPT.from_pretrained("gpt2")
@@ -322,6 +341,7 @@ model.to(device)
 
 
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # cosine decay learning rate scheduler with warmup
 # there are many different learning rate schedulers
 max_lr = 6e-4
@@ -342,22 +362,24 @@ def get_lr(it):
     return min_lr + coeff * (max_lr - min_lr)
 
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device_type=device)
 
-# adamW optimizer is an alternative to the stochastic gradient descent optimizer
-# a normalization that happens on each gradient element individually and speeds up optimization
-# optimizes faster than SGD with learning rate=3e-4
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-88)
 for step in range(max_steps):
     t0 = time.time()
-    x, y = train_loader.next_batch()
-    x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
+    loss_accum = 0.0
 
-    # makes it so that the forward pass uses bfloat16 precision and the code runs super fast on
-    # my 4070 super HOLY MOLY
-    with torch.autocast(device_type='cuda', dtype=torch.bfloat16):
-        logits, loss = model(x, y)
-    loss.backward()
+    for micro_step in range(grad_accum_steps):
+        x, y = train_loader.next_batch()
+        x, y = x.to(device), y.to(device)
+        # makes it so that the forward pass uses bfloat16 precision and the code runs super fast on
+        # my 4070 super HOLY MOLY
+        with torch.autocast(device_type='cuda', dtype=torch.bfloat16):
+            logits, loss = model(x, y)
+        loss = loss / grad_accum_steps # normalizing the lost normalizer
+        loss_accum += loss.detach() # detaching the tensor
+        loss.backward()
 
     # clip the gradient norm to prevent exploding gradients
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
@@ -372,7 +394,7 @@ for step in range(max_steps):
     t1 = time.time()
     dt = (t1 - t0)  # time in seconds
     tokens_per_sec = (train_loader.B * train_loader.T) / dt
-    print(f"step {step} | loss: {loss.item()} | lr: {lr:.4e} | norm: {norm:.4f} | d_time: {dt:.2f}ms | token/sec: {tokens_per_sec:.2f}")
+    print(f"step {step} | loss: {loss_accum.item():.6f} | lr: {lr:.4e} | norm: {norm:.4f} | d_time: {dt:.2f}ms | token/sec: {tokens_per_sec:.2f}")
 
 
 
